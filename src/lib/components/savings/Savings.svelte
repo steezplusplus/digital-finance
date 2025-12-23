086595e00c69845c340926f5be285309d43d4f99
Jesse B <>
2025-12-23 15:19:00 -0500

    feat: create and use tooltip component

--- src/lib/components/savings/Savings.svelte
+++ src/lib/components/savings/Savings.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { SavingsType } from '$lib/types';
   import { formatCurrency } from '$lib/utils/formatters';
+  import Tooltip from '$lib/components/tooltip/Tooltip.svelte';
 
   let { data }: { data: SavingsType } = $props();
 </script>
@@ -18,19 +19,31 @@
     </div>
 
     <div class="border px-2 py-1">
-      <h3 class="font-medium">Reshaping Savings</h3>
+      <div class="flex items-center">
+        <h3 class="font-medium">Reshaping Savings</h3>
+        <span class="ml-auto">
+          <Tooltip description={data.reshaping.tooltip} />
+        </span>
+      </div>
+
       <h4>{data.reshaping.description}</h4>
-      <p>TODO: Tooltip</p>
       <p>{formatCurrency(data.reshaping.amount, data.reshaping.currency)} per month</p>
       <p>{formatCurrency(data.reshaping.yearlyEquivalent, data.reshaping.currency)} per year</p>
     </div>
 
     <div class="border px-2 py-1 md:col-span-2 lg:col-span-1">
-      <h3 class="font-medium">Anomalies</h3>
+      <div class="flex items-center">
+        <h3 class="font-medium">Anomalies</h3>
+        <span class="ml-auto">
+          <Tooltip description={data.reshaping.tooltip} />
+        </span>
+      </div>
+
       <h4>{data.anomalies.description}</h4>
-      <p>TODO: Tooltip</p>
       <p>{data.anomalies.count} Detected issues</p>
-      <p>{formatCurrency(data.anomalies.estimatedCostImpact, data.anomalies.currency)} estimated impact</p>
+      <p>
+        {formatCurrency(data.anomalies.estimatedCostImpact, data.anomalies.currency)} estimated impact
+      </p>
       <p>{data.anomalies.severity.high} High</p>
       <p>{data.anomalies.severity.medium} Medium</p>
       <p>{data.anomalies.severity.high} Low</p>
