5ded677ea73fcbd64a29235a9100e6a2c1a832d5
Jesse B <>
2025-12-23 15:05:49 -0500

    feat: fill out ui for savings

--- src/lib/components/savings/Savings.svelte
+++ src/lib/components/savings/Savings.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { SavingsType } from '$lib/types';
+  import { formatCurrency } from '$lib/utils/formatters';
 
   let { data }: { data: SavingsType } = $props();
 </script>
@@ -7,18 +8,32 @@
 <section class="border px-2 py-1">
   <h2 class="text-lg font-semibold">Savings</h2>
 
-  <div class="grid grid-cols-1 gap-2 md:grid-cols-3">
+  <div class="grid gap-2 md:grid-cols-2 lg:grid-cols-3">
     <div class="border px-2 py-1">
-      <h3 class="text-sm font-medium">Available Savings</h3>
+      <h3 class="font-medium">Available Savings</h3>
+      <h4>{data.available.description}</h4>
+      <p>{formatCurrency(data.available.amount, data.available.currency)} per month</p>
+      <p>{formatCurrency(data.available.yearlyEquivalent, data.available.currency)} per year</p>
+      <p>TODO: available.actionLabel and available.actionUrl</p>
     </div>
 
     <div class="border px-2 py-1">
-      <h3 class="text-sm font-medium">Reshaping Savings</h3>
+      <h3 class="font-medium">Reshaping Savings</h3>
+      <h4>{data.reshaping.description}</h4>
+      <p>TODO: Tooltip</p>
+      <p>{formatCurrency(data.reshaping.amount, data.reshaping.currency)} per month</p>
+      <p>{formatCurrency(data.reshaping.yearlyEquivalent, data.reshaping.currency)} per year</p>
     </div>
 
-    <div class="border px-2 py-1">
-      <h3 class="text-sm font-medium">Anomalies</h3>
+    <div class="border px-2 py-1 md:col-span-2 lg:col-span-1">
+      <h3 class="font-medium">Anomalies</h3>
+      <h4>{data.anomalies.description}</h4>
+      <p>TODO: Tooltip</p>
+      <p>{data.anomalies.count} Detected issues</p>
+      <p>{formatCurrency(data.anomalies.estimatedCostImpact, data.anomalies.currency)} estimated impact</p>
+      <p>{data.anomalies.severity.high} High</p>
+      <p>{data.anomalies.severity.medium} Medium</p>
+      <p>{data.anomalies.severity.high} Low</p>
     </div>
   </div>
-  <pre>{JSON.stringify(data, null, 4)}</pre>
 </section>
