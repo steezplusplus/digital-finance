078649ee3088cc33f0513e41f829e446cdd4c631
Jesse B <>
2025-12-26 12:04:49 -0500

    feat: basic table

--- src/routes/_components/reservations/Reservations.svelte
+++ src/routes/_components/reservations/Reservations.svelte
@@ -1,10 +1,49 @@
 <script lang="ts">
   import type { ReservationType } from '$lib/types';
+  import { formatCurrency, formatDate } from '$lib/utils/formatters';
 
   let { reservations }: { reservations: ReservationType[] } = $props();
 
+  const tableHeaders = [
+    { label: 'TYPE', key: 'type' },
+    { label: 'INSTANCE', key: 'instance' },
+    { label: 'END DATE', key: 'endDate' },
+    { label: 'COST PER HOUR', key: 'costPerHour' },
+    { label: 'MTD SAVINGS', key: 'mtdSavings' }
+  ];
 </script>
 
 <section class="border px-2 py-1">
   <h2 class="text-lg font-semibold">Reservations</h2>
+
+  <table class="w-full">
+    <thead>
+      <tr>
+        {#each tableHeaders as header (header.key)}
+          <th class="text-left tracking-wider">{header.label}</th>
+        {/each}
+      </tr>
+    </thead>
+    <tbody class="divide-y divide-gray-200">
+      {#each reservations as reservation (reservation.id)}
+        <tr>
+          <td>
+            {reservation.type}
+          </td>
+          <td>
+            {reservation.instance}
+          </td>
+          <td>
+            {formatDate(reservation.endDate)}
+          </td>
+          <td>
+            {formatCurrency(reservation.costPerHour, reservation.currency)}
+          </td>
+          <td>
+            {formatCurrency(reservation.mtdSavings, reservation.currency)}
+          </td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
 </section>
