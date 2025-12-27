5a8b65dfb79c62bdc582d8514e1d99e0ac559592
Jesse B <>
2025-12-27 14:16:50 -0500

    feat: copypasta chart

--- src/routes/_components/spend/Spend.svelte
+++ src/routes/_components/spend/Spend.svelte
@@ -1,10 +1,35 @@
 <script lang="ts">
+  import * as echarts from 'echarts';
   import type { SpendType } from '$lib/types';
+  import { onMount } from 'svelte';
 
   let { spend }: { spend: SpendType } = $props();
+
+  const option = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: [150, 230, 224, 218, 135, 147, 260],
+        type: 'line'
+      }
+    ]
+  };
+
+  onMount(() => {
+    const chartDom = document.getElementById('chart');
+    const myChart = echarts.init(chartDom);
+    option && myChart.setOption(option);
+  });
 </script>
 
 <section class="border px-2 py-1">
   <h2 class="text-xl font-semibold">Spend Overview</h2>
-  <pre>{JSON.stringify(spend, null, 4)}</pre>
+  <!-- <pre>{JSON.stringify(spend, null, 4)}</pre> -->
+  <div id="chart" class="h-100 w-full"></div>
 </section>
