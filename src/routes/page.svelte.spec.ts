50fe887a2f7770dce71411bffad9e0480a1ea016
Jesse B <>
2025-12-28 16:43:53 -0500

    test: update test

--- src/routes/page.svelte.spec.ts
+++ src/routes/page.svelte.spec.ts
@@ -2,7 +2,30 @@ import { page } from 'vitest/browser';
 import { describe, expect, it } from 'vitest';
 import { render } from 'vitest-browser-svelte';
 import Page from './+page.svelte';
-import type { CloudCostDataType, SavingsType, UtilizationType } from '$lib/types';
+import type { CloudCostDataType, SavingsType, SpendType, UtilizationType } from '$lib/types';
+
+const mockSpend: SpendType = {
+  projected: {
+    amount: 28477,
+    currency: 'USD',
+    percentageChange: 46.65,
+    trend: 'up',
+    comparedTo: 'previous_period'
+  },
+  current: {
+    amount: 17316,
+    currency: 'USD',
+    period: 'last_30_days',
+    periodLabel: 'Last 30 Days'
+  },
+  timeSeries: [
+    {
+      date: '2024-11-19',
+      amount: 412.5,
+      projected: 420
+    }
+  ]
+};
 
 const mockUtilizationData: UtilizationType = {
   organizationName: 'North AI',
@@ -50,7 +73,7 @@ const mockCloudCostData: CloudCostDataType = {
   meta: {},
   period: {},
   reservations: [],
-  spend: {},
+  spend: mockSpend,
   savings: mockSavingsData,
   tableConfig: {},
   utilization: mockUtilizationData
