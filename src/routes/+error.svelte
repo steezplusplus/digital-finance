b2926e34f879cb417f56cc64019deb4e8a225ef6
Jesse B <>
2025-12-29 12:53:44 -0500

    feat: Show error status code

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,1 +1,6 @@
-<h2 class="text-lg font-semibold">An error has occurred. Try reloading.</h2>
+<script lang="ts">
+    import { page } from '$app/state';
+</script>
+
+<h2 class="text-lg font-semibold">{page.status}</h2>
+<h3 class="font-semibold">An error has occurred. Try reloading.</h3>
