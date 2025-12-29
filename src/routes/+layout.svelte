2f9293fbb3f921b004901b93cd0597d990b5fdab
Jesse B <>
2025-12-29 12:53:32 -0500

    style: cleanup styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,6 @@
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
 <main class="mx-auto max-w-7xl space-y-4 p-4 sm:px-6 lg:px-8">
-  <h1 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Digital Finance</h1>
+  <h1 class="text-3xl font-bold tracking-tight">Digital Finance</h1>
   {@render children()}
 </main>
