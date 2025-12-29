6e0b18ac03841170fb0a82a93e3e3a785f0da272
Jesse B <>
2025-12-29 13:02:07 -0500

    feat: loading and error ui

--- src/routes/+error.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-    import { page } from '$app/state';
-</script>
-
-<h2 class="text-lg font-semibold">{page.status}</h2>
-<h3 class="font-semibold">An error has occurred. Try reloading.</h3>
